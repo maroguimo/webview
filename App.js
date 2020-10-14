3cef4bb01b4bff28ed63222c5d268319cdceedc1
Matheus Moreira <matheus@unidatanet.com.br>
2020-10-14 06:37:36 -0300

    Criação da logo do app e da splash screen

--- App.js
+++ App.js
@@ -1,38 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import Constants from 'expo-constants';
-import React, { Component } from 'react';
-import { StyleSheet, View, TouchableWithoutFeedback } from 'react-native';
-import { WebView } from 'react-native-webview';
+import { StatusBar } from "expo-status-bar";
+import Constants from "expo-constants";
+import React, { Component } from "react";
+import { StyleSheet, View, TouchableWithoutFeedback } from "react-native";
+import { WebView } from "react-native-webview";
 
 export default class App extends Component {
-  render() {
-    const INJECTEDJAVASCRIPT = "document.body.style.userSelect = ‘none'";
+	render() {
+		const INJECTEDJAVASCRIPT = "document.body.style.userSelect = ‘none'";
 
-    return (
-      <View style={styles.container}>
-        <TouchableWithoutFeedback 
-          accessible={false}
-          onLongPress={()=>alert('opss...')}>
-          <WebView
-            injectedJavaScript={INJECTEDJAVASCRIPT}
-            source={{
-              uri:
-                'http://unidata.unidataautomacao.com.br',
-            }}
-          />
-          </TouchableWithoutFeedback>
-          <StatusBar style="inverted" />
-      </View>
-      
-    );
-  }
+		return (
+			<View style={styles.container}>
+				<TouchableWithoutFeedback
+					accessible={false}
+					onLongPress={() => alert("Clique do usuário...")}
+				>
+					<WebView
+						injectedJavaScript={INJECTEDJAVASCRIPT}
+						source={{
+							uri: "http://unidata.unidataautomacao.com.br",
+						}}
+					/>
+				</TouchableWithoutFeedback>
+				<StatusBar style="inverted" />
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#3f51b5',
-    justifyContent: 'center'
-  },
+	container: {
+		flex: 1,
+		paddingTop: Constants.statusBarHeight,
+		backgroundColor: "#3f51b5",
+		justifyContent: "center",
+	},
 });
